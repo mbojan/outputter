5d85e31e688a04cbf359479ab50c24a945cb07d1
Michal Bojanowski <michal2992@gmail.com>
2018-08-14 18:29:37 +0200

    Testing data frame sink

---
+++ tests/testthat/test-out-df.R
@@ -0,0 +1,49 @@
+context("Testing data frame sink")
+
+
+test_that("repeated calls with scalars are silent and give proper output", {
+  out <- make_output(
+    data.frame(a=numeric(0), b=numeric(0))
+    )
+  expect_silent(
+    for(i in 1:5) {
+      out(a=i, b=-i)
+    }
+  )
+  expect_silent(
+    r <- out()
+  )
+  expect_equivalent(
+    r,
+    data.frame(a=1:5, b=seq(-1, -5))
+  )
+})
+
+
+
+
+
+
+test_that("repeated calls with vectors work", {
+  out <- make_output(
+    data.frame(iter=numeric(0), b=numeric(0))
+  )
+  expect_silent(
+    for(i in 1:5) {
+      out(
+        iter = i,
+        b = 1:2
+      )
+    }
+  )
+  expect_silent(
+    r <- out()
+  )
+  expect_equivalent(
+    r,
+    data.frame(
+      iter = rep(1:5, each=2),
+      b = rep(1:2, 5)
+    )
+  )
+})
